646da5ade13df29ff1a1f493b92ad1e2a602dd70
shoff <k4r1mch1k@gmail.com>
2020-05-11 21:22:01 +0500

    second commit

--- js/script.js
+++ js/script.js
@@ -3,69 +3,7 @@
 // если "анек" то в <p> ответ бота вставляется анек;
 // создание нового блока с аргументом <p> и value,
 // создает новый блок "all messages" с сообщением юзера и ответом бота;
-
-var startBtn = document.querySelector(".start-btn");
-startBtn.addEventListener("click", startMsg);
-
-function startMsg ()
-{
-  var value = this.value;
-  var allmsgs = document.querySelector(".all-messages");
-  var divUser = document.createElement('div');
-    divUser.className = "user-message";
-  var pUser = document.createElement('p');
-    pUser.className = "your-message";
-    pUser.innerHTML = value;
-    divUser.append(pUser);
-    allmsgs.append(divUser);
-  var divBot = document.createElement('div');
-    divBot.className = "bots-messages";
-  var pBot = document.createElement('p');
-    pBot.className = "bot-message";
-    pBot.innerHTML = "Что будем делать?";
-    divBot.append(pBot);
-    allmsgs.append(divBot);
-  var startBtnForm = document.querySelector(".start");
-    startBtnForm.style.display = "none";
-  var actionsForm = document.querySelector(".actions");
-    actionsForm.style.display = "block";
-}
-
-var actionBtn = document.querySelectorAll(".action-btn");
-actionBtn.forEach(i => i.addEventListener("click", actionMsg));
-
-function actionMsg()
-{
-  var actionsForm = document.querySelector(".actions");
-
-  var value = this.value;
-  var allmsgs = document.querySelector(".all-messages");
-  var divUser = document.createElement('div');
-    divUser.className = "user-message";
-  var pUser = document.createElement('p');
-    pUser.className = "your-message";
-    pUser.innerHTML = value;
-    divUser.append(pUser);
-    allmsgs.append(divUser);
-  var divBot = document.createElement('div');
-    divBot.className = "bots-messages";
-  var pBot = document.createElement('p');
-    pBot.className = "bot-message";
-    if(value == "Хочу посмеяться") {
-      pBot.innerHTML = "Выбирай категорию шутки";
-    var categoriesBtnForm = document.querySelector(".categories");
-      categoriesBtnForm.style.display = "block";
-    }
-      
-    if(value == "У меня нет настроения, ацтань!")
-      pBot.innerHTML = "Заходи, когда оно появится";
-  divBot.append(pBot);
-  allmsgs.append(divBot);
-  
-  actionsForm.style.display = "none";
-  
-}
-
+var allmsgs = document.querySelector(".all-messages");
 
 var imgMass = [
   "<img src='img/mem-0.jpg'>",
@@ -81,8 +19,8 @@ var imgMass = [
 ];
 
 var vdsMass = [
-  "<video><source src='vds/movie-0.mp4'></video>",
-  "<video><source src='vds/movie-1.mp4'></video>",
+  "<video controls width='250'><source src='vds/movie-0.mp4'></video>",
+  "<video controls width='250'><source src='vds/movie-1.mp4'></video>",
   "<video><source src='vds/movie-2.mp4'></video>",
   "<video><source src='vds/movie-3.mp4'></video>",
   "<video><source src='vds/movie-4.mp4'></video>"
@@ -102,49 +40,78 @@ var anekMass = [
   "Попадают как-то немец, голландец и русский на необитаемый остров. русский говорит: – как по-немецки шлюпка? а по-голландски? немец и голландец отвечают: – звідки нам знати. ми через vpn сидимо"
 ];
 
-var categoryBtn = document.querySelectorAll(".category-btn");
-categoryBtn.forEach(i => i.addEventListener("click", category));
 
-function category ()
-{
-  var randomIndex = Math.floor(Math.random() * 10);
 
-  var value = this.value;
+var actionBtn = document.querySelectorAll(".btn");
+actionBtn.forEach(i => i.addEventListener("click", createUserBlock));
 
-  var allmsgs = document.querySelector(".all-messages");
+function createUserBlock(){ //функция создания блока юзера
+  var value = this.value; //get value from btn
   var divUser = document.createElement('div');
-    divUser.className = "user-message";
-  var pUser = document.createElement('p');
-    pUser.className = "your-message";
-    pUser.innerHTML = value;
-    divUser.append(pUser);
+
+  divUser.className = "user-message";
+
+  var pUser = document.createElement('div');
+
+  pUser.className = "your-message";
+  pUser.innerHTML = value;
+  divUser.append(pUser); //insert into nyjnoe place
   allmsgs.append(divUser);
+  createBotBlock(this); 
+}
 
+function createBotBlock(category){
+  var randomIndex = Math.floor(Math.random() * 10);
+  var what = '';
+
+  switch(category.value){
+    case "Привет!":
+      document.querySelector(".start").style.display = "none";
+      document.querySelector(".actions").style.display = "block";
+      break;
+    case "В начало":
+      document.querySelector(".start").style.display = "none";
+      document.querySelector(".actions").style.display = "block";
+      document.querySelector(".categories").style.display = "none";
+      break;
+    case "Давай поболтаем?":
+      what = "рандомный факт про твою жирную мамашу";
+      botMes(what);
+      break;
+    case "Хочу посмеяться":
+      document.querySelector(".actions").style.display = "none";
+      document.querySelector(".categories").style.display = "block";
+      what = "Выбирай категорию шутки";
+      botMes(what);
+      break;
+    case "У меня нет настроения, ацтань!":
+      what = "Заходи, когда оно появится! Я всегда тебе рад!";
+      botMes(what);
+      break;
+    case "анекдот":
+      what = anekMass[randomIndex];
+      botMes(what);
+      break;
+    case "мем":
+      what = imgMass[randomIndex];
+      botMes(what);
+      break;
+    case "видео":
+      what = vdsMass[randomIndex];
+      botMes(what);
+      break;
+  }
+}
+function botMes(what){
   var divBot = document.createElement('div');
-    divBot.className = "bots-messages";
-  var pBot = document.createElement('p');
-    pBot.className = "bot-message";
-
-// АНЕКДОТ АНЕКДОТ АНЕКДОТ АНЕКДОТ АНЕКДОТ АНЕКДОТ АНЕКДОТ
-// =====================================================
-  if (value == "анекдот") {
-    pBot.innerHTML = anekMass[randomIndex];
-  }
-
-// МЕМ МЕМ МЕМ МЕМ МЕМ МЕМ МЕМ МЕМ МЕМ 
-// =====================================================
-  if (value == "мем") {
-    pBot.innerHTML = imgMass[randomIndex];
-  }
-
-// ВИДЕО ВИДЕО ВИДЕО ВИДЕО ВИДЕО ВИДЕО ВИДЕО ВИДЕО
-// =====================================================
-  if (value == "видео") {
-    pBot.innerHTML = vdsMass[randomIndex]
-  }
-
+  divBot.className = "bots-messages";
+  var pBot = document.createElement('div');
+  pBot.className = "bot-message";
+  pBot.innerHTML = what;
   divBot.append(pBot);
   allmsgs.append(divBot);
-
-  
+  divBot.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start'
+  })
 }
